8bb74895ab523bf3357a1cdf01ab5a8b2ca48657
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-03-28 16:05:04 +0200

    git hup action push

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,4 @@ class FruitsMarket extends StatelessWidget {
         home: OnBoardingView());
   }
 }
+//https://accounts.google.com/o/oauth2/auth?client_id=563584335869-fgrhgmd47bqnekij5i8b5pr03ho849e6.apps.googleusercontent.com&scope=email%20openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloudplatformprojects.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffirebase%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform&response_type=code&state=602116697&redirect_uri=http%3A%2F%2Flocalhost%3A9005
